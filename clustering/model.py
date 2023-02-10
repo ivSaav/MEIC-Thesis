797baae5dccb3d314ffdaec15d8e89e0791f7e5e
Ivo Saavedra <ivo_saavedra@outlook.com>
2023-02-10 12:20:29 +0000

    Finished basic training pipeline structure (training not working) Add logging

--- clustering/model.py
+++ clustering/model.py
@@ -20,7 +20,7 @@ class RegressionHyperModel(HyperModel):
 
     def build(self, hp):
         model = Sequential()
-        model.add(layers.Flatten(input_shape=input_shape))
+        model.add(layers.Flatten(input_shape=self.input_shape))
 
         for i in range(hp.Int('layers',5,100)):
           model.add(layers.Dense(units=hp.Int('units_' + str(i), 16, 128, step=4), activation=hp.Choice('act_' + str(i), ['relu', 'tanh', 'sigmoid'])))
@@ -42,53 +42,53 @@ class RegressionHyperModel(HyperModel):
 
 
 
-#load data and scalers
-data_inputs = pd.read_csv("./MULTI_VP_profiles_Compiled/inputsdata_compilation.csv")
-data_outputs = pd.read_csv("./MULTI_VP_profiles_Compiled/outputs_compiled.csv")
-scaler_inputs = load(open('./Models/scaler_inputs.pkl', 'rb'))
-scaler_outputs = load(open('./Models/scaler_outputs.pkl', 'rb'))
-print("loaded data and scalers from disk")
+# #load data and scalers
+# data_inputs = pd.read_csv("./MULTI_VP_profiles_Compiled/inputsdata_compilation.csv")
+# data_outputs = pd.read_csv("./MULTI_VP_profiles_Compiled/outputs_compiled.csv")
+# scaler_inputs = load(open('./Models/scaler_inputs.pkl', 'rb'))
+# scaler_outputs = load(open('./Models/scaler_outputs.pkl', 'rb'))
+# print("loaded data and scalers from disk")
 
 
-#splitting and normalizing the data
+# #splitting and normalizing the data
 
-trainX, testX, trainY, testY = train_test_split(data_inputs, data_outputs, test_size=0.15, random_state=1)
-trainX, valX, trainY, valY = train_test_split(data_inputs, data_outputs, test_size=0.15, random_state=1)
-
-
-testX = scaler_inputs.transform(testX)
-trainX = scaler_inputs.transform(trainX)
-testY = scaler_outputs.transform(testY)
-trainY = scaler_outputs.transform(trainY)
-#print("Inputs: Train \n",testX, trainX.shape, "\n Test \n", testX, testX.shape)
-#print("Outputs: Train \n", trainY, trainY.shape, "\n Test\n", testY, testY.shape)
-print("split and normalized data")
+# trainX, testX, trainY, testY = train_test_split(data_inputs, data_outputs, test_size=0.15, random_state=1)
+# trainX, valX, trainY, valY = train_test_split(data_inputs, data_outputs, test_size=0.15, random_state=1)
 
 
-
+# testX = scaler_inputs.transform(testX)
+# trainX = scaler_inputs.transform(trainX)
+# testY = scaler_outputs.transform(testY)
+# trainY = scaler_outputs.transform(trainY)
+# #print("Inputs: Train \n",testX, trainX.shape, "\n Test \n", testX, testX.shape)
+# #print("Outputs: Train \n", trainY, trainY.shape, "\n Test\n", testY, testY.shape)
+# print("split and normalized data")
 
 
 
-#--------------------Random Search--------------------
-#hypermodel training
-input_shape = (trainX.shape[1],)
-hypermodel = RegressionHyperModel(input_shape)
 
-tuner_rs = RandomSearch(
-            hypermodel,
-            objective='mse',
-            seed=42,
-            max_trials=100,
-            executions_per_trial=1,
-            overwrite=True
-            )
 
-tuner_rs.search(trainX, trainY, epochs=500, validation_split=0.2, verbose=1)  #epochs=500
 
-best_model_r = tuner_rs.get_best_models(num_models=1)[0]
-loss_r, mse_r = best_model_r.evaluate(testX, testY)
-print("Random Search")
-print(best_model_r.summary())
-print(loss_r,mse_r)
-best_model_r.save("./Models/Tuner_Models/random_search_model")
+# #--------------------Random Search--------------------
+# #hypermodel training
+# input_shape = (trainX.shape[1],)
+# hypermodel = RegressionHyperModel(input_shape)
+
+# tuner_rs = RandomSearch(
+#             hypermodel,
+#             objective='mse',
+#             seed=42,
+#             max_trials=100,
+#             executions_per_trial=1,
+#             overwrite=True
+#             )
+
+# tuner_rs.search(trainX, trainY, epochs=500, validation_split=0.2, verbose=1)  #epochs=500
+
+# best_model_r = tuner_rs.get_best_models(num_models=1)[0]
+# loss_r, mse_r = best_model_r.evaluate(testX, testY)
+# print("Random Search")
+# print(best_model_r.summary())
+# print(loss_r,mse_r)
+# best_model_r.save("./Models/Tuner_Models/random_search_model")
 
--- clustering/train_eval.py
+++ clustering/train_eval.py
@@ -6,8 +6,13 @@ from typing import List, Dict, Tuple
 from collections import OrderedDict
 import numpy as np
 from sklearn.preprocessing import QuantileTransformer
+from sklearn.model_selection import train_test_split
 
-# from model import RegressionHyperModel
+import logging
+
+from keras_tuner import RandomSearch
+
+from model import RegressionHyperModel
 
 input_cols = ['R [Rsun]', 'B [G]', 'alpha [deg]']
 output_cols = ['n [cm^-3]', 'v [km/s]', 'T [MK]']
@@ -31,50 +36,85 @@ def load_original_data(data_path: Path) -> Dict[str, pd.DataFrame]:
     
     return OrderedDict({f.stem : df for f, df in zip(filenames, all_data_df)})
 
-def join_files_in_cluster(cluster_files: List[Path], input_cols: List[str], output_cols: List[str]) -> pd.DataFrame:
+def join_files_in_cluster(cluster_files: List[Path], original_data : Dict[str, pd.DataFrame]) -> pd.DataFrame:
     """Join all files in a cluster into a single dataframe."""
-    cluster_df = pd.DataFrame(columns=[input_cols+output_cols])
+    cluster_df = pd.DataFrame(columns=input_cols+output_cols)
     
     for f in cluster_files:
-        df = pd.read_csv(f, skiprows=2, usecols=input_cols+output_cols)
-        cluster_df = pd.concat([cluster_df, df], axis=0)
+        cluster_df = pd.concat([cluster_df, original_data[f]], axis=0, ignore_index=True)
+        
+    print(cluster_df)
+    print(cluster_df.shape)
+    print(cluster_df.columns)
 
-    scaler = QuantileTransformer()
-    inputs_df = cluster_df[[input_cols]]
-    inputs_df = scaler.fit_transform(inputs_df)
-    
-    scaler = QuantileTransformer()
-    outputs_df = cluster_df[[output_cols]]
-    outputs_df = scaler.fit_transform(outputs_df)
-    
-    return inputs_df, outputs_df
+    return cluster_df.iloc[:, 0:3].copy(), cluster_df.iloc[:, 3:].copy()
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('--data', '-d', type=str, required=True)
+    parser.add_argument('--data', '-d', type=Path, required=True)
+    parser.add_argument('--model-output', '-m', type=Path, required=True)
     parser.add_argument('--clusters-files', '-c', type=str, default='./conf')
     
     opts = vars(parser.parse_args())
     
     conf_files = [f for f in Path(opts['clusters_files']).iterdir() if f.is_file()]
     
+    if not opts['model_output'].exists():
+        opts['model_output'].mkdir(parents=True, exist_ok=False)
+    
     print('Preparing data...')
-    data_dict = load_original_data(Path(opts['data']))
+    data_dict = load_original_data(opts['data'])
     
     for f in conf_files:
         print(f'Running {f}...')
         
         with open(f, 'rb') as cf:
             cluster_runs = load(cf)
+            
+        out_dir = opts['model_output'] / f.stem
+        if not out_dir.exists():
+            out_dir.mkdir(parents=True, exist_ok=False)
+        
+        logging.basicConfig(filename=out_dir / 'train.log', format='%(asctime)s - %(levelname)s - %(message)s')
     
         # test each run
-        for run in cluster_runs:
+        for run_id, run in enumerate(cluster_runs):
             # train model on each cluster
             for cluster_id, cluster in run.items():
                 
-                cluster_inputs, cluster_outputs = join_files_in_cluster(cluster, input_cols, output_cols)
+                try:
+                    cluster_inputs, cluster_outputs = join_files_in_cluster(cluster, data_dict)
+                    trainX, testX, trainY, testY = train_test_split(cluster_inputs, cluster_outputs, test_size=0.15, random_state=1)
+                    
+                    print(trainX.shape, trainY.shape)
 
-                model = RegressionHyperModel()
+                    hypermodel = RegressionHyperModel((trainX.shape[1],))
+                    
+                    print((trainX.shape[1],))
+                    
+                    tuner_rs = RandomSearch(
+                        hypermodel,
+                        objective='mse',
+                        seed=42,
+                        max_trials=10,
+                        executions_per_trial=1,
+                        overwrite=True
+                    )
+
+                    tuner_rs.search(trainX, trainY, epochs=500, validation_split=0.2, verbose=1)  #epochs=500
+
+                    best_model_r = tuner_rs.get_best_models(num_models=1)[0]
+                    loss_r, mse_r = best_model_r.evaluate(testX, testY)
+                    print(f"Random Search - {f.stem} : {cluster_id}")
+                    print(best_model_r.summary())
+                    print(loss_r,mse_r)
+                    
+                    logging.info(f'{f.stem}_run{run_id}_c{cluster_id} - loss: {loss_r} - mse: {mse_r}')
+                    logging.info(f'{f.stem}_run{run_id}_c{cluster_id} - {best_model_r.summary()}')
+                    
+                    best_model_r.save(out_dir / f'{f.stem}_{cluster_id}.h5')
+                except Exception as e:
+                    logging.error(f'Error in {f.stem}_run{run_id}_c{cluster_id}', exc_info=True)
 
                 
             
