b2973f092f3003aa79b5321cf6c4c558cbc96c0d
Ivo Saavedra <ivo_saavedra@outlook.com>
2023-02-11 22:17:28 +0000

    Finished training pipeline

--- clustering/data_merge.py
+++ clustering/data_merge.py
@@ -16,6 +16,7 @@ broken_files = set()
 
 
 data_inputs = pd.DataFrame()
+data_outputs = pd.DataFrame()
 data_ns = pd.DataFrame().astype(float)
 data_vs = pd.DataFrame().astype(float)
 data_ts = pd.DataFrame().astype(float)
@@ -41,62 +42,71 @@ for idx, filename in enumerate(files):
     continue
   
   # select input and output columns
-  n = df.iloc[:,4].values.flatten()
-  v = df.iloc[:,5].values.flatten()
-  t = df.iloc[:,6].values.flatten()
+  # n = df.iloc[:,4].values.flatten()
+  # v = df.iloc[:,5].values.flatten()
+  # t = df.iloc[:,6].values.flatten()
+  ouputs = df.iloc[:,[4,5,6]].values.flatten() # get all outputs as an array
   inputs = df.iloc[:,[0,7,9]].values.flatten() # get all inputs as an array
   
-  df_n = pd.Series(n).astype(float)
-  df_v = pd.Series(v).astype(float)
-  df_t = pd.Series(t).astype(float)
+  # df_n = pd.Series(n).astype(float)
+  # df_v = pd.Series(v).astype(float)
+  # df_t = pd.Series(t).astype(float)
+  df_outputs = pd.Series(ouputs).astype(float) #
   df_inputs = pd.Series(inputs).astype(float) # 
   
   # print(df_inputs)
   # exit(0)
 
-  data_ns = pd.concat([data_ns, df_n], ignore_index = True, axis = 1)
-  data_vs = pd.concat([data_vs, df_v], ignore_index = True, axis = 1)
-  data_ts = pd.concat([data_ts, df_t], ignore_index = True, axis = 1)
+  # data_ns = pd.concat([data_ns, df_n], ignore_index = True, axis = 1)
+  # data_vs = pd.concat([data_vs, df_v], ignore_index = True, axis = 1)
+  # data_ts = pd.concat([data_ts, df_t], ignore_index = True, axis = 1)
+  data_outputs = pd.concat([data_outputs, df_outputs], ignore_index = True, axis = 1)
   data_inputs = pd.concat([data_inputs, df_inputs], ignore_index = True, axis = 1)
   
 
 # Transpose dataframes
 data_inputs.columns = [f.stem for f in files if f not in broken_files]
 data_inputs = data_inputs.T
-
 print(data_inputs.head())
-data_ns = data_ns.T
-data_vs = data_vs.T
-data_ts = data_ts.T
+print(data_inputs.shape)
 
-print("N training\n ", data_ns)
+data_outputs.columns = [f.stem for f in files if f not in broken_files]
+data_outputs = data_outputs.T
+print(data_outputs.head())
+print(data_outputs.shape)
 
-data_ns.to_csv(output_path / 'output_n_data_compilation.csv', index=False) 
-data_vs.to_csv(output_path / 'output_v_data_compilation.csv', index=False) 
-data_ts.to_csv(output_path / 'output_t_data_compilation.csv', index=False) 
+# data_ns = data_ns.T
+# data_vs = data_vs.T
+# data_ts = data_ts.T
+
+
+# data_ns.to_csv(output_path / 'output_n_data_compilation.csv', index=False) 
+# data_vs.to_csv(output_path / 'output_v_data_compilation.csv', index=False) 
+# data_ts.to_csv(output_path / 'output_t_data_compilation.csv', index=False) 
 data_inputs.to_csv(output_path / 'inputsdata_compilation.csv', index=True) 
+data_outputs.to_csv(output_path / 'outputsdata_compilation.csv', index=True)
 
 #__________________________________________________________________
 #juntar os outputs de treino num ficheiro n1,v1,t1, n2,v2,t2, ...
-df_n = pd.read_csv(output_path / 'output_n_data_compilation.csv')
-df_v = pd.read_csv(output_path / 'output_v_data_compilation.csv')
-df_t = pd.read_csv(output_path / 'output_t_data_compilation.csv') 
-df_outputs = pd.DataFrame()  
+# df_n = pd.read_csv(output_path / 'output_n_data_compilation.csv')
+# df_v = pd.read_csv(output_path / 'output_v_data_compilation.csv')
+# df_t = pd.read_csv(output_path / 'output_t_data_compilation.csv') 
+# df_outputs = pd.DataFrame()  
 
-print(len(df_n),df_n.shape[1])
+# print(len(df_n),df_n.shape[1])
 
-#print(df_n.head())
-for col in range(0,640): 
-    column = str(col)
-    n_col = df_n[column]
-    v_col = df_v[column]
-    t_col = df_t[column]
-    if (col == 0): 
-        df_outputs = pd.concat([n_col,v_col], axis = 1)
-        df_outputs = pd.concat([df_outputs, t_col], axis = 1)
-    else: 
-        df_outputs = pd.concat([df_outputs, n_col, v_col, t_col], axis = 1)
+# #print(df_n.head())
+# for col in range(0,640): 
+#     column = str(col)
+#     n_col = df_n[column]
+#     v_col = df_v[column]
+#     t_col = df_t[column]
+#     if (col == 0): 
+#         df_outputs = pd.concat([n_col,v_col], axis = 1)
+#         df_outputs = pd.concat([df_outputs, t_col], axis = 1)
+#     else: 
+#         df_outputs = pd.concat([df_outputs, n_col, v_col, t_col], axis = 1)
     
-print(df_outputs)
-print(len(df_outputs),df_outputs.shape[1])
-df_outputs.to_csv(output_path / 'outputs_compiled.csv', index=False) 
+# print(df_outputs)
+# print(len(df_outputs),df_outputs.shape[1])
+# df_outputs.to_csv(output_path / 'outputs_compiled.csv', index=False) 
--- clustering/train_eval.py
+++ clustering/train_eval.py
@@ -2,11 +2,14 @@ from argparse import ArgumentParser
 from pathlib import Path
 import pandas as pd
 from pickle import load, dump
+import json
 from typing import List, Dict, Tuple
 from collections import OrderedDict
 import numpy as np
 from sklearn.preprocessing import QuantileTransformer
 from sklearn.model_selection import train_test_split
+
+from keras import backend as K
 
 import logging
 
@@ -20,37 +23,54 @@ output_cols = ['n [cm^-3]', 'v [km/s]', 'T [MK]']
 def load_original_data(data_path: Path) -> Dict[str, pd.DataFrame]:
     """Load the original data from the file."""
     
-    filenames = [f for f in data_path.iterdir()]
-    all_data_df = pd.DataFrame(columns=input_cols+output_cols)
-    for f in filenames:
-        all_data_df = pd.concat([all_data_df, 
-                   pd.read_csv(f, skiprows=2, usecols=input_cols+output_cols)], axis=0)
-        
-    scaler = QuantileTransformer()
-    scaler.fit(all_data_df)
-    all_data_df = scaler.transform(all_data_df)
-    all_data_df = pd.DataFrame(all_data_df, columns=input_cols+output_cols)
-        
-    # separate into file series
-    all_data_df = [all_data_df.iloc[i*640:i*640+640, :] for i in range(len(all_data_df) // 640)]
+    inputs = pd.read_csv(data_path / 'inputsdata_compilation.csv')
+    inputs.rename({'Unnamed: 0': 'filename'}, axis=1, inplace=True)
+    outputs = pd.read_csv(data_path / 'outputsdata_compilation.csv')
+    outputs.rename({'Unnamed: 0': 'filename'}, axis=1, inplace=True)    
     
-    return OrderedDict({f.stem : df for f, df in zip(filenames, all_data_df)})
+    input_filenames = inputs[['filename']]
+    output_filenames = outputs[['filename']]
+    
+    scaler_inputs, scaler_ouputs = QuantileTransformer(), QuantileTransformer()
+    inputs = scaler_inputs.fit_transform(inputs.iloc[:, 1:])
+    outputs = scaler_ouputs.fit_transform(outputs.iloc[:, 1:])
+    
+    inputs = pd.DataFrame(inputs)
+    inputs = pd.concat([input_filenames, inputs], axis=1)
+    
+    outputs = pd.DataFrame(outputs)
+    outputs = pd.concat([output_filenames, outputs], axis=1)
+    
+    print("Scaled inputs:", inputs.head())
+    print("Scaled outputs:", outputs.head())
+    return inputs, outputs
 
-def join_files_in_cluster(cluster_files: List[Path], original_data : Dict[str, pd.DataFrame]) -> pd.DataFrame:
+def join_files_in_cluster(cluster_files: List[Path], input_data : pd.DataFrame, output_data : pd.DataFrame) -> pd.DataFrame:
     """Join all files in a cluster into a single dataframe."""
     cluster_inputs, cluster_outputs = pd.DataFrame(), pd.DataFrame()
     
-    print("cluster size", len(cluster_files))
-    for idx, f in enumerate(cluster_files):
-        cluster_inputs = pd.concat([cluster_inputs, original_data[f].iloc[:, 0:3]], axis=1, ignore_index=True)
-        cluster_outputs = pd.concat([cluster_outputs, original_data[f].iloc[:, 3:]], axis=1, ignore_index=True)        
+    for f in cluster_files:
+        cluster_inputs = pd.concat(
+                            [
+                                cluster_inputs, 
+                                input_data.loc[input_data['filename'] == f].iloc[:, 1:]
+                            ],
+                            axis=0, ignore_index=True)
+        cluster_outputs = pd.concat(
+                            [
+                                cluster_outputs, 
+                                output_data.loc[output_data['filename'] == f].iloc[:, 1:]
+                            ], 
+                            axis=0, ignore_index=True)        
     
-    print(cluster_inputs.head())
-    print(cluster_inputs.shape)
+    # print(cluster_inputs.head())
+    # print(cluster_inputs.shape)
     # print(cluster_df)
     # print(cluster_df.shape)
     # print(cluster_df.columns)
+    print("Cluster shape:", cluster_inputs.shape)
 
+    # TODO val split
     return cluster_inputs, cluster_outputs
 
 if __name__ == '__main__':
@@ -67,68 +87,72 @@ if __name__ == '__main__':
         opts['model_output'].mkdir(parents=True, exist_ok=False)
     
     print('Preparing data...')
-    data_dict = load_original_data(opts['data'])
+    inputs, outputs = load_original_data(opts['data'])
+    
+    logging.basicConfig(filename=opts['model_output'] / 'train.log', format='%(asctime)s - %(levelname)s - %(message)s')
     
     for f in conf_files:
-        print(f'Running {f}...')
-        
         with open(f, 'rb') as cf:
             cluster_runs = load(cf)
             
         out_dir = opts['model_output'] / f.stem
         if not out_dir.exists():
             out_dir.mkdir(parents=True, exist_ok=False)
-        
-        logging.basicConfig(filename=out_dir / 'train.log', format='%(asctime)s - %(levelname)s - %(message)s')
-    
+                    
         # test each run
+        stats = dict()
         for run_id, run in enumerate(cluster_runs):
+            stats[int(run_id)] = list()
             # train model on each cluster
             for cluster_id, cluster in run.items():
-                
+                print('____________________________________________________')
+                print(f'Running {f.stem} run {run_id} cluster {cluster_id}')
                 try:
-                    cluster_inputs, cluster_outputs = join_files_in_cluster(cluster, data_dict)
+                    cluster_inputs, cluster_outputs = join_files_in_cluster(cluster, inputs, outputs)
                     trainX, testX, trainY, testY = train_test_split(cluster_inputs, cluster_outputs, test_size=0.15, random_state=1)
                     
                     print(trainX.shape, trainY.shape)
 
                     hypermodel = RegressionHyperModel((trainX.shape[1],))
                     
-                    print((trainX.shape[1],))
-                    
                     tuner_rs = RandomSearch(
                         hypermodel,
                         objective='mse',
                         seed=42,
-                        max_trials=10,
+                        max_trials=5,
                         executions_per_trial=1,
-                        overwrite=True
+                        overwrite=True,
+                        directory='./tuner_search', project_name=f.stem + f'_run{run_id}_{cluster_id}'
                     )
 
-                    tuner_rs.search(trainX, trainY, epochs=500, validation_split=0.2, verbose=1)  #epochs=500
+                    tuner_rs.search(trainX, trainY, epochs=500, validation_split=0.2, verbose=0)  #epochs=500
 
                     best_model_r = tuner_rs.get_best_models(num_models=1)[0]
                     loss_r, mse_r = best_model_r.evaluate(testX, testY)
-                    print(f"Random Search - {f.stem} : {cluster_id}")
-                    print(best_model_r.summary())
-                    print(loss_r,mse_r)
+                    print(f"Random Search - {f.stem} : run{run_id} : {cluster_id}")
+                    # print(best_model_r.summary())
+                    print(f'loss: {loss_r} | mse: {mse_r}')
                     
-                    logging.info(f'{f.stem}_run{run_id}_c{cluster_id} - loss: {loss_r} - mse: {mse_r}')
-                    logging.info(f'{f.stem}_run{run_id}_c{cluster_id} - {best_model_r.summary()}')
+                    best_model_r.save(out_dir / f'{f.stem}_run{run_id}_{cluster_id}.h5')
                     
-                    best_model_r.save(out_dir / f'{f.stem}_{cluster_id}.h5')
+                    # save stats in global file
+                    stats[run_id].append({
+                        'run': int(run_id),
+                        'cluster': int(cluster_id),
+                        'loss': float(loss_r),
+                        'mse': float(mse_r),
+                        'cluster_size': len(cluster),
+                    })
+                    
+                    
+                    with open(out_dir / 'stats.json', 'w') as stats_f:
+                        json.dump(stats, stats_f, indent=3)
+                    
+                    # clear models from memory 
+                    if K.backend() == 'tensorflow':
+                        K.clear_session()
+                                            
                 except Exception as e:
                     logging.error(f'Error in {f.stem}_run{run_id}_c{cluster_id}', exc_info=True)
+                    print(f'Error in {f.stem}_run{run_id}_c{cluster_id}', e)
                     exit(1)
-
-                
-            
-            
-                
-            
-            
-        
-        
-        
-    
-    
