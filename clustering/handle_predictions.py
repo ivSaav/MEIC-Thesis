50dc6a6d4d1ee81c853b11bcc25dab4ebfe15ac1
Ivo Saavedra <ivo_saavedra@outlook.com>
2023-02-23 16:43:55 +0000

    Add pytorch dataset and dataloader for multivp code cleanup

--- clustering/handle_predictions.py
+++ clustering/handle_predictions.py
@@ -5,7 +5,6 @@ from pickle import load
 from argparse import ArgumentParser
 
 from tools.data import load_original_data, join_files_in_cluster, plot_cluster_preds
-from sklearn.metrics import mean_squared_error
 
 if __name__ == '__main__':
     argparser = ArgumentParser()
@@ -35,55 +34,42 @@ if __name__ == '__main__':
         model = load_model(opts['hist_path'] / f"models/{opts['cluster_file']}" / model_file)
         
         inputs, _outputs = join_files_in_cluster(cluster, original_in, original_out)
-        
         predictions = model.predict(inputs)
-        print(predictions.shape)
         
         cluster_filenames.extend(cluster)
         all_predictions.append(pd.DataFrame(predictions))
     
     all_predictions = pd.concat(all_predictions, ignore_index=True)
     
-    # mse = mean_squared_error(original_out.iloc[:, 1:], all_predictions)
-    
     # do inverse tranform on the predictions
     all_predictions.columns = all_predictions.columns.astype(str)
     all_predictions = scaler_out.inverse_transform(all_predictions)
     
-    
-    print(all_predictions.shape)
+    print("Preds shape: ", all_predictions.shape)
     print(all_predictions)
-    preds_dir = out_dir / 'data'
-    if not preds_dir.exists() and not opts['no_write']:
-        preds_dir.mkdir(parents=True)
     
-    compiled_in = pd.read_csv(opts['data_path'] / 'inputsdata_compilation.csv') 
-    for f, pred in zip(cluster_filenames, all_predictions):
-        ns, vs, ts = list(pred[:640]), list(pred[640:1280]), list(pred[1280:])
-        outputs = pd.DataFrame({'n [cm^-3]': ns, 'v [km/s]': vs, 'T [MK]': ts})
+    # save predictions in separate files
+    if not opts['no_write']:
+        preds_dir = out_dir / 'data'
+        if not preds_dir.exists() and not opts['no_write']:
+            preds_dir.mkdir(parents=True)
         
-        inputs = compiled_in.loc[compiled_in['filename'] == f].iloc[:, 1:].values[0]
-        rs, bs, alphas = list(inputs[:640]), list(inputs[640:1280]), list(inputs[1280:])
-        inputs = pd.DataFrame({'R [Rsun]': rs, 'B [G]': bs, 'alpha [deg]': alphas})
+        compiled_in = pd.read_csv(opts['data_path'] / 'inputsdata_compilation.csv') 
+        for f, pred in zip(cluster_filenames, all_predictions):
+            ns, vs, ts = list(pred[:640]), list(pred[640:1280]), list(pred[1280:])
+            outputs = pd.DataFrame({'n [cm^-3]': ns, 'v [km/s]': vs, 'T [MK]': ts})
+            
+            inputs = compiled_in.loc[compiled_in['filename'] == f].iloc[:, 1:].values[0]
+            rs, bs, alphas = list(inputs[:640]), list(inputs[640:1280]), list(inputs[1280:])
+            inputs = pd.DataFrame({'R [Rsun]': rs, 'B [G]': bs, 'alpha [deg]': alphas})
 
-        df = pd.concat([inputs, outputs], axis=1)
-        df.to_csv(preds_dir / f'{f}.csv', index=False)
+            df = pd.concat([inputs, outputs], axis=1)
+            df.to_csv(preds_dir / f'{f}.csv', index=False)
         
-        
-    
-    # all_predictions = pd.DataFrame(all_predictions)
-    # if opts['plots']:
-    #     plot_cluster_preds(all_predictions, opts['cluster_file'], out_dir)
-    # all_predictions = pd.concat([pd.DataFrame(cluster_filenames, columns=["filename"]), all_predictions], axis=1)
-    # if not opts['no_write']:    
-    #     print(all_predictions.head())
-    #     all_predictions.to_csv(out_dir / f"predictions_compiled.csv", index=False)
-    
-    # # print(original_out.shape, all_predictions.shape)
-    # # print("MSE: ", mse)
-    
-    # with open('metrics.txt', 'a') as f:
-    #     f.write(f"{opts['cluster_file']} : {opts['run_id']} : {mse}\n")
+    # plot predictions
+    if opts['plots']:
+        all_predictions = pd.DataFrame(all_predictions)
+        plot_cluster_preds(all_predictions, opts['cluster_file'], out_dir)
         
     
     
--- clustering/model.py
+++ clustering/model.py
@@ -1,17 +1,6 @@
-import pandas as pd
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-import keras_tuner as kt
 from keras_tuner import HyperModel
-from keras_tuner import RandomSearch, Hyperband, BayesianOptimization
-from keras import models, layers
-from sklearn.preprocessing import StandardScaler
+from keras import layers
 from keras.models import Sequential
-from numpy.random import seed
-from keras.utils.vis_utils import plot_model
-from pickle import load
-from sklearn.model_selection import train_test_split
 
 # Define parameters of HyperModel 
 class RegressionHyperModel(HyperModel):
@@ -38,57 +27,3 @@ class RegressionHyperModel(HyperModel):
         )
     
         return model
-
-
-
-
-# #load data and scalers
-# data_inputs = pd.read_csv("./MULTI_VP_profiles_Compiled/inputsdata_compilation.csv")
-# data_outputs = pd.read_csv("./MULTI_VP_profiles_Compiled/outputs_compiled.csv")
-# scaler_inputs = load(open('./Models/scaler_inputs.pkl', 'rb'))
-# scaler_outputs = load(open('./Models/scaler_outputs.pkl', 'rb'))
-# print("loaded data and scalers from disk")
-
-
-# #splitting and normalizing the data
-
-# trainX, testX, trainY, testY = train_test_split(data_inputs, data_outputs, test_size=0.15, random_state=1)
-# trainX, valX, trainY, valY = train_test_split(data_inputs, data_outputs, test_size=0.15, random_state=1)
-
-
-# testX = scaler_inputs.transform(testX)
-# trainX = scaler_inputs.transform(trainX)
-# testY = scaler_outputs.transform(testY)
-# trainY = scaler_outputs.transform(trainY)
-# #print("Inputs: Train \n",testX, trainX.shape, "\n Test \n", testX, testX.shape)
-# #print("Outputs: Train \n", trainY, trainY.shape, "\n Test\n", testY, testY.shape)
-# print("split and normalized data")
-
-
-
-
-
-
-# #--------------------Random Search--------------------
-# #hypermodel training
-# input_shape = (trainX.shape[1],)
-# hypermodel = RegressionHyperModel(input_shape)
-
-# tuner_rs = RandomSearch(
-#             hypermodel,
-#             objective='mse',
-#             seed=42,
-#             max_trials=100,
-#             executions_per_trial=1,
-#             overwrite=True
-#             )
-
-# tuner_rs.search(trainX, trainY, epochs=500, validation_split=0.2, verbose=1)  #epochs=500
-
-# best_model_r = tuner_rs.get_best_models(num_models=1)[0]
-# loss_r, mse_r = best_model_r.evaluate(testX, testY)
-# print("Random Search")
-# print(best_model_r.summary())
-# print(loss_r,mse_r)
-# best_model_r.save("./Models/Tuner_Models/random_search_model")
-
