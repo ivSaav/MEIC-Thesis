09e766b67f3f3e65c2d8da91a7d53d3ef39650d1
Ivo Saavedra <ivo_saavedra@outlook.com>
2023-04-05 11:40:07 +0100

    Started AAE clustering methods

--- anomaly/models/aae_pyramid.py
+++ anomaly/models/aae_pyramid.py
@@ -6,6 +6,8 @@ import torch
 from torch import optim
 
 from typing import Tuple
+import random
+import gc as gc
 
 from tools.viz import plot_data_values
 
@@ -17,6 +19,8 @@ def init_weights(model : nn.Sequential, slope=0.2):
             if idx+1 >= len(model): continue # last layer
             
             actv = model[idx+1]._get_name()
+            if "BatchNorm" in actv: actv = model[idx+2]._get_name()
+            
             if actv == "LeakyReLU":
                 nn.init.kaiming_normal_(layer.weight, a=slope)
             elif actv == "Sigmoid":
@@ -118,10 +122,33 @@ class Discriminator(nn.Module):
 
     def forward(self, z):
         return self.model(z)
+    
+
+def gaussian_sample(nsamples, gopts):
+    # determine x and y coordinates of the target mixture of gaussians
+    x_centroid = (gopts.radius * np.sin(np.linspace(0, 2 * np.pi, gopts.nclusters, endpoint=False)) + 1) / 2
+    y_centroid = (gopts.radius * np.cos(np.linspace(0, 2 * np.pi, gopts.nclusters, endpoint=False)) + 1) / 2
+    # determine each gaussians mean (centroid) and standard deviation
+    centroids = np.vstack([x_centroid, y_centroid]).T
+    # determine the number of samples to be created per gaussian
+    samples_per_gaussian = nsamples//gopts.nclusters
+
+    target_z = np.empty((0, gopts.dim))
+    # iterate over the number of distinct gaussians
+    
+    point_distr_indexes = set(random.sample(range(gopts.nclusters), nsamples % gopts.nclusters))
+    for i, mu in enumerate(centroids):
+        # place an additional point in current cluster in case of odd nsamples
+        sp = samples_per_gaussian if i not in point_distr_indexes else samples_per_gaussian + 1
+        # create n gaussian groups
+        z_samples = np.random.normal(mu, gopts.sigma, size=(sp, gopts.dim))
+        target_z = np.vstack([target_z, z_samples])  
+    return target_z, centroids
+
 
 
 def train(netEnc : nn.Module, netDec : nn.Module, netD : nn.Module, 
-          dataloader : torch.utils.data.DataLoader, opts, device, model_id : str = ""):
+          dataloader : torch.utils.data.DataLoader, opts, gopts, device):
     
     # Initialize BCELoss function
     criterion = nn.BCELoss()
@@ -141,17 +168,18 @@ def train(netEnc : nn.Module, netDec : nn.Module, netD : nn.Module,
     D_losses = []
     best_loss = np.inf
 
-    print("Starting Training Loop...")
     # For each epoch
+    print("Starting Training Loop...")
     for epoch in range(opts.epochs):
         D_epoch_loss = 0
         G_epoch_loss = 0
         epoch_reconstr_loss = 0
         D_class = 0
         epoch_enc_x = np.empty((0, opts.l_dim))
+        epoch_filenames = []
         
         # For each batch in the dataloader
-        for i, (x, _filename) in enumerate(dataloader):
+        for i, (x, filenames) in enumerate(dataloader):
             bsize = x.shape[0]
             real_labels = torch.full((bsize, 1), real_label, dtype=torch.float, device=device)
             fake_labels = torch.full((bsize, 1), fake_label, dtype=torch.float, device=device)
@@ -162,45 +190,35 @@ def train(netEnc : nn.Module, netDec : nn.Module, netD : nn.Module,
             enc_x = netEnc(real)
             dec_x = netDec(enc_x)
             
-            errG = reconstr_loss(dec_x, real)
+            errG = (reconstr_loss(dec_x, real) + criterion(netD(enc_x), real_labels)) * 0.5
             errG.backward()
             optmEnc.step(), optmDec.step()
             epoch_reconstr_loss += errG.item()
             
-            
             # ===== Train Discriminator =====
             optmD.zero_grad()
             # noise as discriminator ground truth
-            z = torch.rand(bsize, opts.l_dim, device=device)
-            
+            z_sample, _ = gaussian_sample(bsize, gopts)
+            z = torch.from_numpy(z_sample).float().to(device)
             # discriminator ability to classify real from generated samples
             outputs = netD(z)
-            errD_real = criterion(netD(z), real_labels)
+            errD_real = criterion(outputs, real_labels)
             errD_fake = criterion(netD(enc_x.detach()), fake_labels)    
-            errD = errD_real  + errD_fake
+            errD = (errD_real  + errD_fake) * 0.5
             D_class += outputs.mean().item()
             epoch_enc_x = np.vstack((epoch_enc_x, enc_x.detach().cpu().numpy()))
+            epoch_filenames.extend(filenames)
             
             errD.backward()
             optmD.step()
             D_epoch_loss += errD.item()
-            
-            
-            # ===== Regularization =====
-            optmEnc.zero_grad()
-            z_fake = netEnc(real)
-            D_fake = netD(z_fake)
-            
-            errG = criterion(D_fake, real_labels)
-            errG.backward()
-            optmEnc.step()
             G_epoch_loss += errG.item()
         
         D_epoch_loss /= i
         G_epoch_loss /= i
         epoch_reconstr_loss /= i
         D_class /= i
-         
+            
         # Output training stats
         print('[%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(z): %.4f\tDec_Err: %.4f'
                 % (epoch, opts.epochs, D_epoch_loss, G_epoch_loss, D_class, epoch_reconstr_loss),
@@ -215,21 +233,22 @@ def train(netEnc : nn.Module, netDec : nn.Module, netD : nn.Module,
                             title=f"Epoch {epoch} - G loss: {errG.item():.4f} - D loss: {errD.item():.4f}", 
                             labels=["B [G]", "alpha [deg]"],
                             scales={"alpha [deg]" : "symlog"})
-            preffix = f"{model_id}_" if model_id else ""
-            plt.savefig(opts.model_out / "img" / f"{preffix}e{epoch}.png")
+            plt.savefig(opts.model_out / "img" / f"e{epoch}.png")
             plt.close(fig)
             
-            print(epoch_enc_x[:5], epoch_enc_x.shape)
-            print(z)
+            # save scatter of latent representation by the encoder
             fig, ax = plt.subplots(figsize=(10,10))
             plt.scatter(epoch_enc_x[:, 0], epoch_enc_x[:, 1], s=1)
-            plt.savefig(opts.model_out / "img" / f"{preffix}scatter{epoch}.png")
+            plt.savefig(opts.model_out / "img" / f"scatter{epoch}.png")
             plt.close(fig)
+            
+            gc.collect()
             
         if G_epoch_loss < best_loss and epoch > 0:
             best_loss = G_epoch_loss
-            preffix = f"{model_id}_" if model_id else ""
-            torch.save(netEnc.state_dict(), opts.model_out / f'{preffix}Enc.pth')
-            torch.save(netDec.state_dict(), opts.model_out / f'{preffix}Dec.pth')
-            torch.save(netD.state_dict(), opts.model_out / f'{preffix}D.pth')
-    return G_losses, D_losses
+            torch.save(netEnc.state_dict(), opts.model_out / 'Enc.pth')
+            torch.save(netDec.state_dict(), opts.model_out / 'Dec.pth')
+            torch.save(netD.state_dict(), opts.model_out / 'D.pth')
+
+
+
--- anomaly/models/linear.py
+++ anomaly/models/linear.py
@@ -1,7 +1,22 @@
 from torch import nn
 from collections import OrderedDict
 
-class Generator(nn.Module):
+def init_weights(model : nn.Sequential, slope=0.2):
+    # Init weights with xavier uniform distribution to reduce vanishing gradients
+    # https://machinelearningmastery.com/weight-initialization-for-deep-learning-neural-networks/
+    for idx, layer in enumerate(model):
+        if layer._get_name() == "Linear":
+            if idx+1 >= len(model): continue # last layer
+            
+            actv = model[idx+1]._get_name()
+            if actv == "LeakyReLU":
+                nn.init.kaiming_normal_(layer.weight, a=slope)
+            elif actv == "Sigmoid":
+                nn.init.xavier_normal_(layer.weight, 1)
+            elif actv == "Tanh":
+                nn.init.xavier_normal_(layer.weight, 5/3)
+
+class GeneratorPyramid(nn.Module):
     def __init__(self, input_size : int, output_size : int, hidden_size : int, nlower : int = 4, neg_slope : float = 0.01):
         super().__init__()
         self.hidden_size = hidden_size
@@ -36,7 +51,47 @@ class Generator(nn.Module):
             layers.append(("out_bn", nn.BatchNorm1d(output_size)))
             
         self.main = nn.Sequential(OrderedDict(layers))
+        init_weights(self.main)
         # self.main.add_module('tanh', nn.C())
+        
+    def forward(self, x):
+        batch_size = x.size(0)
+        x = self.main(x)
+        x = x.view(batch_size, self.output_size)
+        return x
+    
+class Generator(nn.Module):
+    def __init__(self, input_size : int, output_size : int, hidden_size : int, nlower : int = 4, neg_slope : float = 0.01):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.input_size = input_size
+        self.output_size = output_size
+        self.nlower = nlower
+        
+        def _block(prefix, idx, in_size, out_size):
+            return [
+                (f"{prefix}l{idx}", nn.Linear(in_size, out_size)),
+                # (f"{prefix}bn{idx}", nn.BatchNorm1d(out_size)),
+                (f"{prefix}leaky{idx}", nn.LeakyReLU(neg_slope, inplace=True))
+            ]
+            
+        
+        
+        layers = []
+        # downsample input
+        layers.extend(_block("in", "", input_size, hidden_size))
+        
+        # hidden layers
+        for i in range(1, nlower+1):
+            layers.extend(_block("lin", i, hidden_size, hidden_size))
+        
+        # output layer
+        layers.append(("out", nn.Linear(hidden_size, output_size)))
+        # layers.append(("out_bn", nn.BatchNorm1d(output_size)))
+            
+        self.main = nn.Sequential(OrderedDict(layers))
+        init_weights(self.main)
+        
         
     def forward(self, x):
         batch_size = x.size(0)
@@ -55,18 +110,20 @@ class Discriminator(nn.Module):
         # input layer
         if input_size != hidden_size:
             self.main.add_module('in', nn.Linear(input_size, hidden_size))
-            self.main.add_module('in_bn', nn.BatchNorm1d(hidden_size))
+            # self.main.add_module('in_bn', nn.BatchNorm1d(hidden_size))
             self.main.add_module('in_leaky', nn.LeakyReLU(neg_slope, inplace=True))
         
         # inverted pyramid
-        for i in range(1, nlayers):
+        for i in range(1, nlayers+1):
             self.main.add_module(f'l{i}', nn.Linear(hidden_size//(2**(i-1)), hidden_size//(2**i)))
-            self.main.add_module(f'bn{i}', nn.BatchNorm1d(hidden_size//(2**i)))
+            # self.main.add_module(f'bn{i}', nn.BatchNorm1d(hidden_size//(2**i)))
             self.main.add_module(f'leaky{i}', nn.LeakyReLU(neg_slope, inplace=True))
             
-        pyramid_out_size = self.main.get_submodule(f'l{nlayers-1}').out_features
+        pyramid_out_size = self.main.get_submodule(f'l{nlayers}').out_features
         self.main.add_module('out', nn.Linear(pyramid_out_size, 1))
         self.main.add_module('sigmoid', nn.Sigmoid())
+        init_weights(self.main)
+        
         
     def forward(self, x):
         batch_size = x.size(0)
