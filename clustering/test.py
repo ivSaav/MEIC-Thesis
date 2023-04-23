f5e5bd367a38675ec2ff142ca9016f8e83d37342
Ivo Saavedra <ivo_saavedra@outlook.com>
2023-04-23 09:35:23 +0100

    Add clustering test script

--- clustering/test.py
+++ clustering/test.py
@@ -8,9 +8,12 @@ clusters = [f for f in Path("local").iterdir() if f.is_file()]
 for conf in clusters:
     with open(conf, 'rb') as cf:
         all_runs = load(cf)
+        
+    
 
     for run_dict in all_runs:
         run_id = run_dict['run_id']
+        # else: continue
 
         print("Executing: ", f"python train_eval.py -d ../data/compiled -m models -cf {conf} -r {run_id}")
         os.system(f"python train_eval.py -d ../data/compiled -m models -cf {conf} -r {run_id}") 
