bb66f4cc3f677437ee2e2b4e6d2c1bd956cf9a92
Ivo Saavedra <ivo_saavedra@outlook.com>
2023-05-06 14:43:25 +0100

    clustering validation techniques

--- clustering/notebooks/tools.py
+++ clustering/notebooks/tools.py
@@ -1,8 +1,12 @@
 import pickle as pkl
 import math
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
 import numpy as np
 from scipy.cluster.hierarchy import dendrogram
+from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_harabasz_score, fowlkes_mallows_score, rand_score
 import seaborn as sns
 import pandas as pd
 
@@ -117,3 +121,89 @@ def plot_cluster_file_group(filenames, labels, nclusters):
     fig.legend(loc="center right", title="Cluster")
     ax.get_legend().remove()
     return fig
+
+
+def clustering_metrics(model, data, params) -> pd.DataFrame:
+    scores = {"n_clusters" : [], "silhouette" : [], "davies_bouldin" : [], "calinski_harabasz" : [],}
+    for i in range(2, 20):
+        kmeans = model(n_clusters=i, **params)
+        labels = kmeans.fit_predict(data)
+        scores["n_clusters"].append(i)
+        scores["silhouette"].append(silhouette_score(data, labels))
+        scores["davies_bouldin"].append(davies_bouldin_score(data, labels))
+        # scores["rand"].append(rand_score(data, labels))
+        scores["calinski_harabasz"].append(calinski_harabasz_score(data, labels))
+        # scores["fowlkes_mallows"].append(fowlkes_mallows_score(data, labels))
+    return pd.DataFrame(scores, index=None)  
+
+
+def silhouette_plot(X, nclusters, model, params):
+    for n_clusters in range(2, nclusters+1):
+        # Create a subplot with 1 row and 2 columns
+        fig, ax1 = plt.subplots(figsize=(5,5))
+
+        # The 1st subplot is the silhouette plot
+        # The silhouette coefficient can range from -1, 1 but in this example all
+        # lie within [-0.1, 1]
+        ax1.set_xlim([-0.1, 1])
+        # The (n_clusters+1)*10 is for inserting blank space between silhouette
+        # plots of individual clusters, to demarcate them clearly.
+        ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
+
+        # Initialize the clusterer with n_clusters value and a random generator
+        # seed of 10 for reproducibility.
+        clusterer = model(n_clusters=n_clusters, **params)
+        cluster_labels = clusterer.fit_predict(X)
+
+        # The silhouette_score gives the average value for all the samples.
+        # This gives a perspective into the density and separation of the formed
+        # clusters
+        silhouette_avg = silhouette_score(X, cluster_labels)
+        print(
+            "For n_clusters =",
+            n_clusters,
+            "The average silhouette_score is :",
+            silhouette_avg,
+        )
+
+        # Compute the silhouette scores for each sample
+        sample_silhouette_values = silhouette_samples(X, cluster_labels)
+
+        y_lower = 10
+        for i in range(n_clusters):
+            # Aggregate the silhouette scores for samples belonging to
+            # cluster i, and sort them
+            ith_cluster_silhouette_values = sample_silhouette_values[cluster_labels == i]
+
+            ith_cluster_silhouette_values.sort()
+
+            size_cluster_i = ith_cluster_silhouette_values.shape[0]
+            y_upper = y_lower + size_cluster_i
+
+            color = cm.nipy_spectral(float(i) / n_clusters)
+            ax1.fill_betweenx(
+                np.arange(y_lower, y_upper),
+                0,
+                ith_cluster_silhouette_values,
+                facecolor=color,
+                edgecolor=color,
+                alpha=0.7,
+            )
+
+            # Label the silhouette plots with their cluster numbers at the middle
+            ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+            # Compute the new y_lower for next plot
+            y_lower = y_upper + 10  # 10 for the 0 samples
+
+        ax1.set_title(f"Silhouette plot ({i} clusters)")
+        ax1.set_xlabel("The silhouette coefficient values")
+        ax1.set_ylabel("Cluster label")
+
+        # The vertical line for average silhouette score of all the values
+        ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+
+        ax1.set_yticks([])  # Clear the yaxis labels / ticks
+        ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+
+        
